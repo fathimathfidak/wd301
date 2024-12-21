6a8e29db5dffa40718cb6b50d2215464587b2637
Fida <fathimathfidak849@gmail.com>
2024-12-21 13:10:40 -0500

    Create TaskCard.tsx

---
+++ TaskCard.tsx
@@ -0,0 +1,33 @@
+interface TaskCardProps {
+  dueDate: string;
+  completedAtDate: string;
+  assigneeName: string;
+}
+
+const TaskCard: React.FC<TaskCardProps> = ({ dueDate, completedAtDate, assigneeName }) => {
+  // component code
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<TaskCard 
+  dueDate="2024-12-31" 
+  completedAtDate="2024-12-21" 
+  assigneeName="John Doe" 
+/>
